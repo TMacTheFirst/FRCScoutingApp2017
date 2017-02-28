1d748b8163a50da6fa9355c980b209a8a570e4e5
Taylor McCorkill <tmccorkill@mitre.org>
2017-02-28 18:05:43 -0500

    Some layouts

--- app/src/main/java/com/example/frcscoutingapp2017/MainActivity.java
+++ app/src/main/java/com/example/frcscoutingapp2017/MainActivity.java
@@ -2,14 +2,29 @@ package com.example.frcscoutingapp2017;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
-
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.linear_scouting_page);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayList<String> spinnerArray = new ArrayList<String>();
+        spinnerArray.add("No Rating");
+        spinnerArray.add("Green");
+        spinnerArray.add("Yellow");
+        spinnerArray.add("Red");
+
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, spinnerArray);
+        spinner.setAdapter(spinnerArrayAdapter);
+
     }
 }
